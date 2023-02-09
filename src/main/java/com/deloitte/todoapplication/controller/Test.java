959ea6c1aac4857ce2cb6371aded161a4a2c52a0
EricXiaxl <xiaolong_xia9373@163.com>
2023-02-09 14:38:38 +0000

    [Add]test image

--- src/main/java/com/deloitte/todoapplication/controller/Test.java
+++ src/main/java/com/deloitte/todoapplication/controller/Test.java
@@ -4,6 +4,6 @@ import com.deloitte.todoapplication.util.ImageUtil;
 
 public class Test {
     public static void main(String[] args){
-        ImageUtil.blurryDetect("b9.jpg");
+        ImageUtil.blurryDetect("b10.jpg");
     }
 }
